e148d88dfa02124e70f46fc7a0e42274d9c7b06f
jamacan19 <124731983+jamacan19@users.noreply.github.com>
2023-02-09 18:35:49 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ basic.forever(function () {
         maqueen.motorStop(maqueen.Motors.M1)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        basic.pause(1000)
         CuentaManchas += 1
         basic.showNumber(CuentaManchas)
+        basic.pause(1000)
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, randint(50, 150))
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, randint(50, 150))
